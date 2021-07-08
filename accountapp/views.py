d42d93627649e56acbf1d041f24b9fb62baf3cf2
LeeHyunGyu0823 <karisiz700@gmail.com>
2021-07-08 09:29:03 +0900

    2021 07 08 Get Method

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, "accountapp/Base_content.html")
+    if request.method == "POST":
+        return render(request, "accountapp/Base_content.html",
+                      context={"text":"Post Method!"})
+    else:
+        return render(request, "accountapp/Base_content.html",
+                      context={"text":"Get Method!"})
