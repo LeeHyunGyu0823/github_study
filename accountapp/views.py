f86cfe5063322e0dad38b5b17d52d2ac358eb624
LeeHyunGyu0823 <karisiz700@gmail.com>
2021-07-12 09:25:24 +0900

    2021 07 12 Get Method

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,13 +9,15 @@ def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
-
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request, "accountapp/Base_content.html",
-                      context={"hello_world_output": new_hello_world})
+                      context={"hello_world_list": hello_world_list})
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, "accountapp/Base_content.html",
-                      context={"text":"Get Method!"})
+                      context={"hello_world_list":hello_world_list})
