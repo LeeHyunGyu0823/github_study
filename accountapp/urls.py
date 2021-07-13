7d7f2aa43be41ecb12502183253f011beb14384a
LeeHyunGyu0823 <karisiz700@gmail.com>
2021-07-13 16:04:51 +0900

    2021 07 13 templates

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('login/', LoginView.as_view(templates_name='accountapp/login.html'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
          name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
