c528f3b500cc5cdc2f14df26695ec258002a35e5
LeeHyunGyu0823 <karisiz700@gmail.com>
2021-07-27 16:03:25 +0900

    2021 07 27 commit

--- Django/urls.py
+++ Django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.templatetags.static import static
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
