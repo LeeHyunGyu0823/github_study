8524fb362e5998f2a6a33219b4327bfe6da9d371
LeeHyunGyu0823 <karisiz700@gmail.com>
2021-08-05 09:16:57 +0900

    2021 08 02 commit

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,18 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/create.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
 
     def form_valid(self, form):
         form.instance.writer = self.request.user
@@ -24,6 +30,8 @@ class ArticleDetailView(DetailView):
     template_name = 'articleapp/detail.html'
 
 
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'get')
 class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleCreationForm
